a836d822425bb997e8ef93e8b7c0d4045fa16dd3
Will Roberts <will.roberts11@gmail.com>
2017-06-28 00:06:01 -0700

    Adds more colors

--- roguelike/chars.go
+++ roguelike/chars.go
@@ -7,9 +7,22 @@ const (
 	charLight rune = '☺'
 	charDark  rune = '☻'
 
-	colorPink   termbox.Attribute = 0xb8
-	colorWall   termbox.Attribute = 0x11 // MIN
-	colorGround termbox.Attribute = 0xe8 // MAX
+	colorBlack     termbox.Attribute = 0x11
+	colorBrown     termbox.Attribute = 0x60
+	colorGold      termbox.Attribute = 0x90
+	colorGreen     termbox.Attribute = 0x23
+	colorIndigo    termbox.Attribute = 0x40
+	colorLtOrange  termbox.Attribute = 0xe0
+	colorLtPurple  termbox.Attribute = 0xb8
+	colorLtSeafoam termbox.Attribute = 0xa0
+	colorMagenta   termbox.Attribute = 0x80
+	colorPink      termbox.Attribute = 0xd0
+	colorSalmon    termbox.Attribute = 0xb0
+	colorSeafoam   termbox.Attribute = 0x50
+	colorSkyBlue   termbox.Attribute = 0x70
+	colorTeal      termbox.Attribute = 0x20
+	colorWhite     termbox.Attribute = 0xe8
+	colorYellow    termbox.Attribute = 0xc0
 )
 
 var (
