85f1c2cdadc42b7f02fe4f686c82567e428e96c2
Will Roberts <will.roberts11@gmail.com>
2017-06-27 23:29:22 -0700

    Adds more constants

--- roguelike/chars.go
+++ roguelike/chars.go
@@ -1,11 +1,20 @@
 package roguelike
 
+import termbox "github.com/nsf/termbox-go"
+
 const (
 	charRogue rune = '@'
 	charLight rune = '☺'
 	charDark  rune = '☻'
+
+	colorPink termbox.Attribute = 0xb8
 )
 
 var (
 	blockRunes = []rune{' ', '░', '▒', '▓', '█'}
+	neatRunes  = []rune{'※', '⁜', '⁕', '⁂'}
+	// https://en.wikipedia.org/wiki/List_of_Unicode_characters#Block_Elements
+	// https://en.wikipedia.org/wiki/List_of_Unicode_characters#Geometric_Shapes
+	// https://en.wikipedia.org/wiki/List_of_Unicode_characters#Miscellaneous_Symbols
+	// https://en.wikipedia.org/wiki/Emoji#Unicode_blocks
 )
